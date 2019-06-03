966e81f2af262eb34f9164569c93d8d3a59c1fa6
Alison <alisonruiz@usantotomas.edu.co>
2019-06-03 17:18:36 -0500

    funciona2

--- index.js
+++ index.js
@@ -1,26 +1,17 @@
-//Importamos Express tras instalarlo vía NPM
-const express = require("express");
-// Definimos App como la función del módulo Express
-const App = express()
+const express = require('express');
+const app = express();
+const path = require('path');
 
-// Definimos algunas variables que usaremos en las distintas funciones
-const port = process.env.PORT || 3000
-const options = {
-  root: __dirname
-}
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+// app.get('/js_node/suma.js', function(req, res) {
+//     res.sendFile(path.join(__dirname, 'js_node/suma.js'));
+// });
+// app.get('/css_node/index.css', function(req, res) {
+//     res.sendFile(path.join(__dirname, 'css_node/index.css'));
+// });
 
-// Definimos funciones para luego usarlas al recibir una petición en el router
-function getHTML(req, res) {
-  res.sendFile('./index.html', options, (err) => {
-    if (err) throw err;
-    console.log('Sirviendo index.html')
-  })
-}
-
-// Definimos las rutas
-App.get('/', getHTML)
-
-// Escuchamos el puerto de Express
-App.listen(port, () => {
-  console.log('Aplicacion escuchando en el puerto: ' + port)
-})
+app.listen(process.env.PORT || 4000, function(){
+    console.log('Your node js server is running');
+});
